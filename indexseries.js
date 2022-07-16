efda1b32a02d995e0b351f687dfbd02b5782cb57
Anjalisingh00 <anjalisinghftp0000@gmail.com>
2022-07-16 16:55:44 +0530

    hi

---
+++ indexseries.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+ReactDom.render(
+  <>
+  <h1>pick any one Netflix Series</h1>
+  <p>list of five best series</p>
+  <ol>
+  <li> strangers thing</li>
+  <li>games of throne</li>
+  <li>lock and key</li>
+  <li>honted house</li>
+  <li>the love</li>
+  </ol>
+  </>,
+  document.getElementById("root")
+
+
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
